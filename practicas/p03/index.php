9aec2134d18b1416af0b8b71a5c815f49b0cb668
Mmontiel13 <marcmontiel98@gmai.com>
2023-09-05 12:56:43 -0600

    Segundo Ejercicio completado

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -29,6 +29,34 @@
         echo '<li>$_element1 es valida por que inicia con guion bajo.';
         echo '</ul>';
     ?>
-
+    <br><hr>
+    <h2> Ejercicio 2 </h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>
+        $a = “ManejadorSQL”;<br>
+        $b = 'MySQL’;<br>
+        $c = &$a;<br>
+    </p>
+    <?php
+       $a = "ManejadorSQL";
+       $b = 'MySQL';
+       $c = &$a;
+       echo 'Mostrando variables';
+       echo '<br>';
+       var_dump($a);
+       var_dump($b);
+       var_dump($c);
+       $a = "PHP server";
+       $b = &$a;
+       echo '<br>';
+       echo 'Mostrando variables luego de cambiar el valor:';
+       echo '<br>';
+       var_dump($a);
+       var_dump($b);
+       var_dump($c);
+       echo '<br>';
+       echo 'Al cambiar el valor de la variable a y las otras 2 variables tener el valor de la misma tambien se modifico su valor';
+       echo '<br>';
+    ?>
 </body>
 </html>
